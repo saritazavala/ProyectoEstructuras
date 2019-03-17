cd4cccf3f23fc7c0ed51434251c1feafadceb9d2
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-03-17 17:44:07 -0600

    Clase Funciones y tiene un errorcito :anguished:

--- Interprete/src/Analizador.java
+++ Interprete/src/Analizador.java
@@ -4,10 +4,47 @@ Proyecto Estructuras de Datos
 Interprete Lisp
 Marzo de 2019
 * */
-public class Analizador {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
 
-    public String[] obtenerCondicionales(String condicionales){
-        return null;
+public class Analizador {
+    Stack<Funciones> funciones = new Stack<>();
+    Stack<String> operaciones = new Stack<>();
+    Stack<Integer> valorParametros = new Stack<>();
+    boolean existeOperacion = false;
+
+    public void definirFunciones(){
+        operaciones.push("(COND (> 3 2))");
+        valorParametros.push(2);
+        valorParametros.push(3);
+        funciones.push(new Funciones("Sumar", "(a,b)", 2, valorParametros, operaciones));
+    }
+
+    public void realizarFunciones(String funcion){
+        for (int i = 0; i < funciones.size(); i++) {
+            if(funciones.get(i).getNombre().equals(funcion)){
+                funciones.remove(i).realizarFuncion();
+                existeOperacion = true;
+            }
+        }
+        if(existeOperacion == false){
+            System.out.println("No existe ninguna funcion con ese nombre");
+        }
+    }
+
+    String[] parser;
+    String[] divisorNombre;
+    String nombreFuncion;
+    String[] parametros;
+    public void Parser(String funcion){
+        parser = funcion.split(" ");
+        divisorNombre = parser[1].split(" ");
+        parametros = parser[2].split(",");
+        for (int i = 0; i < parser.length ; i++) {
+            System.out.println(parser[i]);
+        }
+        nombreFuncion = divisorNombre[1];
+        System.out.println(nombreFuncion);
     }
 }
-
---
+++ Interprete/src/Funciones.java
@@ -0,0 +1,78 @@
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Funciones {
+    private String nombre;
+    private String parametros;
+    private int cantidadParametros;
+    private Stack<Integer> valores;
+    private Stack<String> operacion;
+
+    public Funciones(String nombre, String parametros, int cantidadParametros, Stack<Integer> valores, Stack<String> operacion){
+        this.nombre = nombre;
+        this.parametros = parametros;
+        this.cantidadParametros = cantidadParametros;
+        this.valores = valores;
+        this.operacion = operacion;
+    }
+
+    public void realizarFuncion(){
+        System.out.println(nombre);
+        System.out.println(parametros);
+        System.out.println("Cantidad de parametros: " + cantidadParametros);
+        System.out.println("a: " + valores.pop().toString() + " - " + "b: " + valores.pop());
+        //System.out.println(operacion.pop());\
+        if(operacion.peek().contains("+") || operacion.peek().contains("-") || operacion.peek().contains("*") || operacion.peek().contains("/")){
+            new OperacionesAritmeticas(operacion.pop());
+        }else if(operacion.peek().contains("COND")){
+            new Condicionales(operacion.pop());
+        }
+
+        if(operacion.size() > 0){
+            realizarFuncion();
+        }else{
+            return;
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getParametros() {
+        return parametros;
+    }
+
+    public void setParametros(String parametros) {
+        this.parametros = parametros;
+    }
+
+    public int getCantidadParametros() {
+        return cantidadParametros;
+    }
+
+    public void setCantidadParametros(int cantidadParametros) {
+        this.cantidadParametros = cantidadParametros;
+    }
+
+    public Stack<Integer> getValores() {
+        return valores;
+    }
+
+    public void setValores(Stack<Integer> valores) {
+        this.valores = valores;
+    }
+
+    public Stack<String> getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(Stack<String> operacion) {
+        this.operacion = operacion;
+    }
+}
